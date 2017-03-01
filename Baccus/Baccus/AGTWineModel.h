a4c94b47213c5fd1d0502fb39584bfc748664aa4
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-01 15:17:42 -0600

    Add properties to the Model

--- Baccus/Baccus/AGTWineModel.h
+++ Baccus/Baccus/AGTWineModel.h
@@ -10,4 +10,14 @@
 
 @interface AGTWineModel : NSObject
 
+@property (strong, nonatomic) NSString *type;
+@property (strong, nonatomic) UIImage *photo;
+@property (strong, nonatomic) NSURL *wineCompanyWeb;
+@property (strong, nonatomic) NSString *notes;
+@property (strong, nonatomic) NSString *origin;
+@property (nonatomic) int rating; //0 - 5
+@property (strong, nonatomic) NSArray *grapes;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *wineCompanyName;
+
 @end
