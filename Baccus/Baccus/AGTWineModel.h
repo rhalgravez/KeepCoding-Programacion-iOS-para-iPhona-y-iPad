3ffc6eb93d13c81cf3243a1748f98a3516d49323
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-01 12:41:28 -0600

    Create Wine model file and cretae prefix header file

---
+++ Baccus/Baccus/AGTWineModel.h
@@ -0,0 +1,13 @@
+//
+//  AGTWineModel.h
+//  Baccus
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/1/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTWineModel : NSObject
+
+@end
---
+++ Baccus/Baccus/AGTWineModel.m
@@ -0,0 +1,13 @@
+//
+//  AGTWineModel.m
+//  Baccus
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/1/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTWineModel.h"
+
+@implementation AGTWineModel
+
+@end
