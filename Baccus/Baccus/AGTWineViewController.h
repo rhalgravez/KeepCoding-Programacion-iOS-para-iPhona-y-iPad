6709735286178ddf259559fed664ddad398c235b
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-02 15:40:22 -0600

    Add the property for the rating

--- Baccus/Baccus/AGTWineViewController.h
+++ Baccus/Baccus/AGTWineViewController.h
@@ -19,6 +19,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *notesLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *photoView;
 
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *ratingViews;
+
 @property(strong, nonatomic) AGTWineModel *model;
 
 -(instancetype) initWithModel:(AGTWineModel *) aModel;
