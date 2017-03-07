74eaae85c3ff2ff3aff3292c62f18e75fb9b88bb
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-07 14:39:09 -0600

    Add AGTWineryModel
    A new model that we are going to use for a TableView in the next commits

---
+++ Baccus/Baccus/AGTWineryModel.h
@@ -0,0 +1,13 @@
+//
+//  AGTWineryModel.h
+//  Baccus
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/7/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTWineryModel : NSObject
+
+@end
---
+++ Baccus/Baccus/AGTWineryModel.m
@@ -0,0 +1,13 @@
+//
+//  AGTWineryModel.m
+//  Baccus
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/7/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import "AGTWineryModel.h"
+
+@implementation AGTWineryModel
+
+@end
