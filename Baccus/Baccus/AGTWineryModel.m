899d5f25739e4e16207efa9d6af8126decdd83cd
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-07 15:29:29 -0600

    Override the init method so the WineryModel can have a deafult wine selection

--- Baccus/Baccus/AGTWineryModel.m
+++ Baccus/Baccus/AGTWineryModel.m
@@ -18,6 +18,48 @@
 
 @implementation AGTWineryModel
 
+#pragma mark - Init
+
+-(instancetype)init {
+    if (self = [super init]) {
+        AGTWineModel *tintorro = [AGTWineModel wineWithName:@"Bembibre"
+                                            wineCompanyName:@"Dominio de Tareas"
+                                                       type:@"tinto"
+                                                     origin:@"El Bierzo"
+                                                     grapes:@[@"Mencía"]
+                                             wineCompanyWeb:[NSURL URLWithString:@"http://www.dominiodetares.com/index.php/es/vinos/baltos/74-bembibrevinos"]
+                                                      notes:@"Este vino muestra toda la complejidad y la elegancia de la variedad Mencía. En fase visual luce un color rojo picota muy cubierto con tonalidades violáceas en el menisco. En nariz aparecen recuerdos frutales muy intensos de frutas rojas (frambuesa, cereza) y una potente ciruela negra, así como tonos florales de la gama de las rosas y violetas, vegetales muy elegantes y complementarios, hojarasca verde, tabaco y maderas aromáticas (sándalo) que le brindan un toque ciertamente perfumado."
+                                                    raiting:5
+                                                      photo:[UIImage imageNamed:@"bembibre"]];
+        
+        AGTWineModel *albarinno = [AGTWineModel wineWithName:@"Zárate"
+                                             wineCompanyName:@"Zárate"
+                                                        type:@"white"
+                                                      origin:@"Rias Bajas"
+                                                      grapes:@[@"Albariño"]
+                                              wineCompanyWeb:[NSURL URLWithString:@"http://www.albarino-zarate.com"]
+                                                       notes:@"El albariño Zarate es un vino blanco monovarietal que pertenece a la Denominación de Origen Rías Baixas. Considerado por la crítica especializada como uno de los grandes vinos blancos del mundo, el albariño ya es todo un mito."
+                                                     raiting:4
+                                                       photo:[UIImage imageNamed:@"zarate"]];
+        
+        AGTWineModel *champagne = [AGTWineModel wineWithName:@"Comtes de Champagne"
+                                             wineCompanyName:@"Champagne Taittinger"
+                                                        type:@"other"
+                                                      origin:@"Champagne"
+                                                      grapes:@[@"Chardonnay"]
+                                              wineCompanyWeb:[NSURL URLWithString:@"http://www.taittinger.fr"]
+                                                       notes:@"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nunc purus. Curabitur eu velit mauris. Curabitur magna nisi, ullamcorper ac bibendum ac, laoreet et justo. Praesent vitae tortor quis diam luctus condimentum. Suspendisse potenti. In magna elit, interdum sit amet facilisis dictum, bibendum nec libero. Maecenas pellentesque posuere vehicula. Vivamus eget nisl urna, quis egestas sem. Vivamus at venenatis quam. Sed eu nulla a orci fringilla pulvinar ut eu diam. Morbi nibh nibh, bibendum at laoreet egestas, scelerisque et nisi. Donec ligula quam, semper nec bibendum in, semper eget dolor. In hac habitasse platea dictumst. Maecenas adipiscing semper rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;"
+                                                     raiting:5
+                                                       photo:[UIImage imageNamed:@"comtesDeChampagne"]];
+        
+        _redWines = @[tintorro];
+        _whiteWines = @[albarinno];
+        _otherWines = @[champagne];
+    }
+    
+    return self;
+}
+
 #pragma mark - Querying the arrays
 
 -(AGTWineModel *)redWineAtIndex:(int)index {
