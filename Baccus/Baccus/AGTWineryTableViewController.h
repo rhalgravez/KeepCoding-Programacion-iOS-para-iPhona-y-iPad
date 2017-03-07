15968c858e0bfeb2abcb74629113167a859226ef
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-07 16:23:04 -0600

    Add a TableViewController

---
+++ Baccus/Baccus/AGTWineryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AGTWineryTableViewController.h
+//  Baccus
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/7/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTWineryTableViewController : UITableViewController
+
+@end
