63346da857e2a5c27f3cd69deee72c49981f53ac
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-07 15:26:30 -0600

    Add methods to get an specific wine

--- Baccus/Baccus/AGTWineryModel.h
+++ Baccus/Baccus/AGTWineryModel.h
@@ -7,12 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "AGTWineModel.h"
 
 @interface AGTWineryModel : NSObject
 
 @property (nonatomic) int redWineCount;
 @property (nonatomic) int whiteWineCount;
 @property (nonatomic) int otherWineCount;
+
+-(AGTWineModel *)redWineAtIndex:(int)index;
+-(AGTWineModel *)whiteWineAtIndex:(int)index;
+-(AGTWineModel *)otherWineAtIndex:(int)index;
 
 @end
--- Baccus/Baccus/AGTWineryModel.m
+++ Baccus/Baccus/AGTWineryModel.m
@@ -18,4 +18,17 @@
 
 @implementation AGTWineryModel
 
+#pragma mark - Querying the arrays
+
+-(AGTWineModel *)redWineAtIndex:(int)index {
+    return [self.redWines objectAtIndex:index];
+}
+
+-(AGTWineModel *)whiteWineAtIndex:(int)index {
+    return [self.whiteWines objectAtIndex:index];
+}
+-(AGTWineModel *)otherWineAtIndex:(int)index {
+    return [self.otherWines objectAtIndex:index];
+}
+
 @end
