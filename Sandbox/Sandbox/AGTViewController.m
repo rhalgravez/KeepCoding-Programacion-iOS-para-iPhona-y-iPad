2309103ba2fdce98378e49d8231eb90503a9990e
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-15 16:05:10 -0600

    Simple example in how you can write to a file

--- Sandbox/Sandbox/AGTViewController.m
+++ Sandbox/Sandbox/AGTViewController.m
@@ -30,7 +30,28 @@
 
 - (IBAction)save:(id)sender {
     
+    //Get cache url folder from Sandbox
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSURL *url = [[fileManager URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] lastObject];
     
+    //Append the file name to get the complete url fot the file test.txt
+    url = [url URLByAppendingPathComponent:@"test.txt"];
+    
+    //Save something in the file
+    NSError *error = nil;
+    BOOL didWrite = NO;
+    didWrite = [@"Hola Muñdo" writeToURL:url atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    
+    if (didWrite == NO) {
+        NSLog(@"Erro when trying to write to the file: %@", error);
+    }
+    
+    //Read file so we can see what it has
+    NSString *resource = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+    
+    if (!resource) {
+        NSLog(@"Erro when trying to read the file: %@", error);
+    }
 }
 
 @end
