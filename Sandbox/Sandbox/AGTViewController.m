b439b0f969d6b3498be2071cf4c2519a7ea6d61d
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-15 15:28:14 -0600

    Add button and its IBAction

--- Sandbox/Sandbox/AGTViewController.m
+++ Sandbox/Sandbox/AGTViewController.m
@@ -14,6 +14,8 @@
 
 @implementation AGTViewController
 
+#pragma mark - Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
@@ -24,14 +26,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Actions
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)save:(id)sender {
+    
+    
 }
-*/
 
 @end
