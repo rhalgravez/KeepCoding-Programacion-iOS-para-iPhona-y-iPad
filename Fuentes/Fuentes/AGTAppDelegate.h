6e2eeee2891b767c45e86f0ecbc4d6573b68d2f9
Roberto Halgravez <roberto.halgravez@globant.com>
2017-03-23 16:25:52 -0600

    Create new project to show the fonts
    Also delete the default Storyboard and ViewController so it an look like an empty app

---
+++ Fuentes/Fuentes/AGTAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fuentes
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/23/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AGTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fuentes/Fuentes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fuentes
+//
+//  Created by Roberto Manuel Halgravez Perea on 3/23/17.
+//  Copyright © 2017 RHalgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AGTAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AGTAppDelegate class]));
+    }
+}
